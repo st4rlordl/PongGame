d124002e31ba4537ad690c7ced6028298dfce8da
st4rlordl <st4rlordepicgame@gmail.com>
2024-09-17 15:37:58 +0200

    add gameover

--- main.lua
+++ main.lua
@@ -3,7 +3,9 @@ _G.love = require("love")
 function love.load()
     love.graphics.setBackgroundColor(150 / 255, 211 / 255, 1)
     width, height = love.graphics.getDimensions()
-
+    math.randomseed(os.time())
+    _G.gameover = true
+    score = 0
     _G.ping = {}
     ping.x = 30
     ping.y = 370
@@ -21,27 +23,58 @@ function love.load()
     _G.ball = {}
     ball.x = width/2
     ball.y = height/2
-    ball.speed = 0.5
-    ball.dx = 200
-    ball.dy = 200
+    ball.dx = 0
+    ball.dy = 0
     ball.r = 15
+
 end
+
+function dir_ball(num)
+    local dir = math.random(2)
+    if dir == 1 then
+        num = -num
+    end
+    return num
+end
+function loose()
+    gameover = true
+    ball.x = width/2
+    ball.y = height/2
+    ball.dx = 0
+    ball.dy = 0
+    ping.x = 30
+    ping.y = 370
+    pong.x = 940
+    pong.y = 370
+    
+end
+function start()
+    gameover = false
+    ball.dx = dir_ball(200)
+    ball.dy = dir_ball(200)
+    score = 0
+end
+
 
 function love.update(dt)
 
-    if love.keyboard.isDown("up") and ping.y >= 0 then
+    -- input
+    if love.keyboard.isDown("up") and ping.y >= 0 and not gameover then
         ping.y = ping.y - ping.speed
     end 
-    if love.keyboard.isDown("down") and ping.y <= height-100 then
+    if love.keyboard.isDown("down") and ping.y <= height-100 and not gameover then
         ping.y = ping.y + ping.speed
     end 
-    if love.keyboard.isDown("w") and pong.y >= 0 then
+    if love.keyboard.isDown("w") and pong.y >= 0 and not gameover then
         pong.y = pong.y - pong.speed
     end 
-    if love.keyboard.isDown("s") and pong.y <= height-100 then
+    if love.keyboard.isDown("s") and pong.y <= height-100 and not gameover then
         pong.y = pong.y + pong.speed
     end 
-    
+    if love.keyboard.isDown("space") and gameover then
+        start()
+    end 
+    -- speed of the ball
     ball.x = ball.x + ball.dx * dt
     ball.y = ball.y + ball.dy * dt
 
@@ -50,24 +83,26 @@ function love.update(dt)
     end
     if ball.x + ball.r >= pong.x and ball.x <= pong.x + pong.width and ball.y <= pong.y + pong.height and ball.y + ball.r >= pong.y then
         ball.dx = -ball.dx
-        
+        score = score + 1
     elseif ball.x + ball.r >= ping.x and ball.x <= ping.x + ping.width and ball.y <= ping.y + ping.height and ball.y + ball.r >= ping.y then
-        ball.dx = -ball.dx
+        ball.dx = -ball.dx --probleme de hitbox
+        score = score + 1
     end
 
    if ball.x + ball.r <= 0 or ball.x + ball.r >= width then
-    --love.graphics.print("GAME OVER", 400, 500)
-    --love.event.quit()
+        loose()
    end
 
 end
 
 function love.draw()
+    love.graphics.setFont(love.graphics.newFont(24))
     textCoord = "X: " .. tostring(ball.x) .. "\nY: " .. tostring(ball.y)
-    textFont = love.graphics.newFont(100)
+    textScore = "Score : " .. tostring(score)
     love.graphics.setColor(0,0,0)
-    love.graphics.print(textCoord, 100, 300)
-
+    love.graphics.print(textCoord, width-24*7, 0)
+    love.graphics.print(textScore, 10, 10)
+    love.graphics.setColor(255,255,255)
     love.graphics.rectangle("fill", ping.x, ping.y, ping.width, ping.height)
     love.graphics.rectangle("fill", pong.x, pong.y, pong.width, pong.height)
 
